a7179821747c3d071223b8c0dd8178f5015c8ae4
syed-dwebbox <144232974+syed-dwebbox@users.noreply.github.com>
2023-10-03 11:38:35 +0530

    Add files via upload

---
+++ src/axios/api.js
@@ -0,0 +1,29 @@
+import { openAxiosInstance,securedAxiosInstance } from "./instance";
+
+const auth_apis = {
+loginWithEmailAndPassword: (email, password) => {
+    const params = new URLSearchParams();
+    params.append('username', email);
+    params.append('password', password);
+    return openAxiosInstance.post(`token`,params)
+  }
+  };
+
+const secured_apis = {
+  getLatestStats:() => {
+    return securedAxiosInstance.get(`/stats/latest`)
+  },
+  postLatestStats:(values) => {
+    return securedAxiosInstance.post(`/stats/`,values)
+  }
+};
+
+
+  
+
+  const apis = {
+    ...auth_apis,
+    ...secured_apis
+  };
+  
+  export default apis;
